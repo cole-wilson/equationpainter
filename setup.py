76d9b7ac84b536efcae4e53bf163d2bccb9dee59
Cole Wilson <cole@colewilson.xyz>
2021-01-28 23:08:00 +0000

    fixed error!!!!!

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except:
 
 options = {
 	"name":"equationpainter",
-	"version":"3.2.9",
+	"version":"3.2.10",
 	"scripts":[],
 	"entry_points":{'console_scripts': ['eqpaint=equationpainter.__main__:main']},
 	"author":"Cole Wilson",
