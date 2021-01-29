51afd0b8f223ea9693c97f0fd4711872f1b54904
Cole Wilson <cole@colewilson.xyz>
2021-01-29 17:17:48 +0000

    a

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except:
 
 options = {
 	"name":"equationpainter",
-	"version":"3.3.11",
+	"version":"3.3.12",
 	"scripts":[],
 	"entry_points":{'console_scripts': ['eqpaint=equationpainter.__main__:main']},
 	"author":"Cole Wilson",
