acdae9f650d892b7f85f2d59b9f3d8fba6bf990e
Cole Wilson <cole@colewilson.xyz>
2021-01-30 23:57:58 +0000

    added more options

--- dist/homebrew/EquationPainter.rb
+++ dist/homebrew/EquationPainter.rb
@@ -1,6 +1,6 @@
 # Created with sailboat, the Python releaser
 
-# v3.3.31
+# v3.3.33
 
 class EquationPainter < Formula
   include Language::Python::Virtualenv
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except:
 
 options = {
 	"name":"equationpainter",
-	"version":"3.3.31",
+	"version":"3.3.33",
 	"scripts":[],
 	"entry_points":{'console_scripts': ['eqpaint=equationpainter.__main__:main']},
 	"author":"Cole Wilson",
