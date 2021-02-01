f98c22acc97f7f05bdd310cfafbda78cd97f50d5
Cole Wilson <cole@colewilson.xyz>
2021-02-01 21:23:15 +0000

    Added .ico file for Windows

--- dist/homebrew/EquationPainter.rb
+++ dist/homebrew/EquationPainter.rb
@@ -1,6 +1,6 @@
 # Created with sailboat, the Python releaser
 
-# v3.3.56
+# v3.3.57
 
 class EquationPainter < Formula
   include Language::Python::Virtualenv
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except:
 
 options = {
 	"name":"equationpainter",
-	"version":"3.3.56",
+	"version":"3.3.57",
 	"scripts":[],
 	"entry_points":{'console_scripts': ['eqpaint=equationpainter.__main__:main']},
 	"author":"Cole Wilson",
